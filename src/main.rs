98b9c85076a05282c580be66139c81eb552d4ff4
lupyuen <luppy@appkaki.com>
2020-09-19 09:34:34 +0800

    Parse command OK

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::env;
 use futures::StreamExt;
 use telegram_bot::*;
 
+/// Listen for commands and handle them
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
@@ -16,21 +17,53 @@ async fn main() -> Result<(), Error> {
     let mut stream = api.stream();
     while let Some(update) = stream.next().await {
         // If the received update contains a new message...
-        let update = update?;
+        let update = update ? ;
         println!("----- {:?}", update);
         if let UpdateKind::Message(message) = update.kind {
             if let MessageKind::Text { ref data, .. } = message.kind {
-                // Show received message
+                //  Show received message
                 println!("-- <{}>: {}", &message.from.first_name, data);
 
-                // Respond to message
-                api.send(message.text_reply(format!(
-                    "Flashing to PineTime at address {}...",
-                    data
-                )))
-                .await?;
+                //  Handle the command
+                handle_command(&api, &message, data)
+                .await ? ;
             }
         }
     }
+    Ok(())
+}
+
+/// Handle a command e.g. "flash - flash 0x0 https://.../firmware.bin"
+async fn handle_command(api: &Api, message: &Message, cmd: &str) -> Result<(), Error> {
+    //  Remove leading and trailing spaces. Replace multiple spaces by 1.
+    let cmd = cmd
+        .trim_start()
+        .trim_end()
+        .replace("  ", " ")
+        .replace("  ", " ")
+        .replace("  ", " ")
+        .replace("  ", " ")
+        .replace("  ", " ");
+    let split: Vec<&str> = cmd.split(' ').collect();
+    let cmd = split[0];
+
+    if cmd != "/flash" {
+        //  Unknown command
+        api.send(message.text_reply(format!(
+            "Unknown command {}. Try /flash 0x0 https://.../firmware.bin",
+            cmd
+        )))
+        .await ? ;
+        return Ok(());
+    }
+
+    //  Handle flash command
+    let addr = split[1];
+    let firmware = split[2];
+    api.send(message.text_reply(format!(
+        "Flashing {} to PineTime at address {}...",
+        firmware, addr
+    )))
+    .await ? ;
     Ok(())
 }
