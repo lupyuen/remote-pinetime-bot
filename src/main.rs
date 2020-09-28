5fceb554766a6f74d01937c5f49f1d87cb106e94
lupyuen <luppy@appkaki.com>
2020-09-28 19:47:13 +0800

    Adding log

--- src/main.rs
+++ src/main.rs
@@ -175,30 +175,27 @@ async fn flash_firmware(addr: &str, path: &str) -> Result<String> {
     Ok(output)
 }
 
-/// Transmit the Semihosting Log to Telegram
-async fn transmit_log() {
-    use std::process::{Command, Stdio};
-    use std::io::{BufRead, BufReader, Error, ErrorKind};
-    
-    fn main() -> Result<(), Error> {
-        let stdout = Command::new("journalctl")
-            .stdout(Stdio::piped())
-            .spawn()?
-            .stdout
-            .ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))?;
-    
-        let reader = BufReader::new(stdout);
-    
-        reader
-            .lines()
-            .filter_map(|line| line.ok())
-            .filter(|line| line.find("usb").is_some())
-            .for_each(|line| println!("{}", line));
-    
-         Ok(())
-    }
-    
+use std::process::{Command, Stdio};
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+
+/// Transmit the Semihosting Log to Telegram    
+async fn transmit_log() -> Result<()> {
+    let stdout = Command::new("journalctl")
+        .stdout(Stdio::piped())
+        .spawn() ?
+        .stdout
+        .ok_or_else(|| Error::new(ErrorKind::Other,"Could not capture standard output."))?;
+
+    let reader = BufReader::new(stdout);
+
+    reader
+        .lines()
+        .filter_map(|line| line.ok())
+        .filter(|line| line.find("usb").is_some())
+        .for_each(|line| println!("{}", line));
+
     //  Wait for "***" and quit with the message
+    Ok(())
 }
 
 /*
